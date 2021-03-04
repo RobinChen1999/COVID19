e96703c5c74ca1b44a42c4c23eb9427bb6e91ca1
Joris Peters <petersjoris98@gmail.com>
2021-03-04 17:54:44 +0100

    Added base for gui

---
+++ MonteCarlo/Gui.py
@@ -0,0 +1,81 @@
+import tkinter as tk
+
+from Simulation import *
+
+
+class Gui:
+    def draw_main_window(self):
+        window = tk.Tk()
+        window.geometry('{}x{}'.format(800, 400))
+
+        # Layout frame
+        frm_layout = tk.Frame(window, bg="red")
+
+        lbl_id_layout = tk.Label(frm_layout, text="Layout Frame")
+        lbl_id_layout.pack()
+
+        # Parameters frame
+        frm_parameters = tk.Frame(window, bg="yellow")
+
+        lbl_id_parameters = tk.Label(frm_parameters, text="Parameters Frame")
+        lbl_id_parameters.pack()
+
+        frm_parameters_input = tk.Frame(frm_parameters, relief=tk.SUNKEN, borderwidth=2)
+        frm_parameters_input.pack()
+
+        # Seed
+        lbl_seed = tk.Label(frm_parameters_input, text="Seed:")
+        ent_seed = tk.Entry(frm_parameters_input, width=20)
+        ent_seed.insert(0, 888892)
+        lbl_seed.grid(row=0, column=0, sticky="e")
+        ent_seed.grid(row=0, column=1)
+
+        # Nr. of Customers
+        lbl_nr_customers = tk.Label(frm_parameters_input, text="Nr. of Customers:")
+        ent_nr_customers = tk.Entry(frm_parameters_input, width=20)
+        ent_nr_customers.insert(0, 10000)
+        lbl_nr_customers.grid(row=1, column=0, sticky="e")
+        ent_nr_customers.grid(row=1, column=1)
+
+        # Max Steps
+        lbl_max_steps = tk.Label(frm_parameters_input, text="Max Steps:")
+        ent_max_steps = tk.Entry(frm_parameters_input, width=20)
+        ent_max_steps.insert(0, 1000)
+        lbl_max_steps.grid(row=2, column=0, sticky="e")
+        ent_max_steps.grid(row=2, column=1)
+
+        # Run button
+        btn_run = tk.Button(frm_parameters,
+                            text="Run",
+                            command=lambda: self.run_simulation(
+                                seed=ent_seed.get(),
+                                nr_customers=ent_nr_customers.get(),
+                                max_steps=ent_max_steps.get()
+                            ))
+        btn_run.pack()
+
+        frm_layout.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+        frm_parameters.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+        window.mainloop()
+
+    def run_simulation(self, seed, nr_customers, max_steps):
+        print("Running simulation with the following parameters:")
+        print(" Seed: ", seed)
+        print(" Nr. of Customers: ", nr_customers)
+        print(" Max Steps:", max_steps)
+
+        sim = Simulation(
+            int(seed),
+            101,
+            101,
+            25,
+            int(nr_customers),
+            outputLevel=0,
+            maxSteps=int(max_steps),
+            probInfCustomer=0.01,
+            probNewCustomer=0.2,
+            imageName="ExampleSuperMarket.pbm",
+            useDiffusion=1,
+            dx=1.0)
+        sim.runSimulation()
--- MonteCarlo/testSimulation.py
+++ MonteCarlo/testSimulation.py
@@ -1,10 +1,13 @@
 from Simulation import *
+from Gui import *
 
 #~ These two have to be matched as the pixel count (in x- and y-axis) of a .ppm image, if that is imported as the simulation geometry!
 pixNx = 101
 pixNy = 101
 
-## parameters: seed, pixNx, pixNy, N shelves (if no file provided), N customers, ..
-sim = Simulation(888892, pixNx, pixNy, 25, 10000, outputLevel=0, maxSteps=100000, probInfCustomer=0.01, probNewCustomer=0.2,imageName="ExampleSuperMarket.pbm",useDiffusion=1,dx=1.0)
-sim.runSimulation()
+gui = Gui()
+gui.draw_main_window()
 
+## parameters: seed, pixNx, pixNy, N shelves (if no file provided), N customers, ..
+# sim = Simulation(888892, pixNx, pixNy, 25, 10000, outputLevel=0, maxSteps=100000, probInfCustomer=0.01, probNewCustomer=0.2,imageName="ExampleSuperMarket.pbm",useDiffusion=1,dx=1.0)
+# sim.runSimulation()
