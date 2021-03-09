bb1a5b56ad86fe36c60f39e17935022ccc7edef4
Merel <mereleikens@gmail.com>
2021-03-09 12:56:40 +0100

    change gridbox colour on click

--- MonteCarlo/StoreLayout.py
+++ MonteCarlo/StoreLayout.py
@@ -1,28 +1,48 @@
 import tkinter as tk
+import numpy as np
 
 class StoreLayout:
     def __init__(self, frame):
         self.canvas_scale = 500
         self.step = int(self.canvas_scale / 10)
         self.canvas = tk.Canvas (frame, bg="blue", width=self.canvas_scale, height=self.canvas_scale)
+        self.canvas.bind('<Button-1>', self.onCanvasClick)
         self.canvas.pack()
 
+    # draws all elements in the canvas
     def draw_store_layout(self):
-        self.draw_grid()
-        self.draw_shelf(3, 4)
-        
+        self.create_grid()
+        self.draw_grid_lines()        
 
-    def draw_grid(self):
+    # draws black lines to show the grid
+    def draw_grid_lines(self):
         for i in range(self.step, self.canvas_scale, self.step):
             # horizontal line
             self.canvas.create_line(0, i, self.canvas_scale, i)
             # vertical line
             self.canvas.create_line(i, 0, i, self.canvas_scale)
 
-    def draw_shelf(self, col, row):
-        x0 = col * self.step
-        y0 = row * self.step
-        x1 = x0 + self.step
-        y1 = y0 + self.step
-        self.canvas.create_rectangle(x0, y0, x1, y1, fill="black")
+    # creates a matrix of rectangle objects grid[row][column]
+    def create_grid(self):
+        self.grid = []
+        for row in range(0, self.step):
+            x0 = row * self.step
+            x1 = x0 + self.step
+            column = []
+            for col in range(0, self.step):
+                y0 = col * self.step
+                y1 = y0 + self.step
+                column.append(self.canvas.create_rectangle(x0, y0, x1, y1, fill="green"))
+            self.grid.append(column)
+
+    # changes colour of rectangle object from grid
+    def draw_shelf(self, row, col):
+        self.canvas.itemconfigure(self.grid[row][col], fill="pink")
+
+    # onClick event on canvas
+    def onCanvasClick(self, event):
+        row = int(event.x / self.step)
+        col = int(event.y / self.step)
+        self.draw_shelf(row=row, col=col)
+
 
