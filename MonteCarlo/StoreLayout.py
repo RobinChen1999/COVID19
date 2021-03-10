cca0a76204fa4ad42f8df3ddc7aac66403a9568c
Merel <mereleikens@gmail.com>
2021-03-10 21:30:01 +0100

    canvas saves as .png and runs in sim

--- MonteCarlo/StoreLayout.py
+++ MonteCarlo/StoreLayout.py
@@ -5,91 +5,91 @@ from PIL import Image, ImageGrab
 class StoreLayout:
     color_floor = "white"
     color_shelf = "black"
-    canvas_scale = 500
-    step = int(canvas_scale / 10)
+    canvas_width = 200
+    step = int(canvas_width / 10)
 
     def __init__(self, frame):
-        self.canvas = tk.Canvas (frame, width=self.canvas_scale, height=self.canvas_scale)
+        # create canvas
+        self.canvas = tk.Canvas (frame, bg=self.color_floor, 
+                                    width=self.canvas_width, 
+                                    height=self.canvas_width)
         self.canvas.bind('<Button-1>', self.onCanvasClick)
         self.canvas.pack()
 
         # radio buttons to select tool
         self.tool_selec = tk.IntVar()
-        toolErase = tk.Radiobutton(frame, text="Remove shelf", variable=self.tool_selec, value=0)
-        toolErase.pack()
-        toolDraw = tk.Radiobutton(frame, text="Draw shelf", variable=self.tool_selec, value=1)
-        toolDraw.pack()
-        toolDraw.select()
+        tool_remove = tk.Radiobutton(frame, 
+                                    text="Remove shelf", 
+                                    variable=self.tool_selec, 
+                                    value=0)
+        tool_remove.pack()
+        tool_draw = tk.Radiobutton(frame, 
+                                    text="Draw shelf", 
+                                    variable=self.tool_selec, 
+                                    value=1)
+        tool_draw.pack()
+        tool_draw.select()
+
+        # button to show/hide the grid
+        self.show_grid = tk.IntVar()
+        btn_grid = tk.Checkbutton(frame,
+                                text="Show Grid", 
+                                variable=self.show_grid, 
+                                onvalue=1, offvalue=0, 
+                                command=self.hide_grid_lines)
+        btn_grid.pack()
+
         
-
-    
-
+        
     # draws all elements in the canvas
     def draw_store_layout(self):
-        self.create_grid()
-        # self.draw_shelf(2, 9, "yellow")       
+        self.draw_grid_lines()
+        self.hide_grid_lines()
 
     # draws black lines to show the grid
     def draw_grid_lines(self):
-        for i in range(self.step, self.canvas_scale, self.step):
+        for i in range(self.step, self.canvas_width, self.step):
             # horizontal line
-            self.canvas.create_line(0, i, self.canvas_scale, i)
+            self.canvas.create_line(0, i, self.canvas_width, i, tags=("grid_lines"))
             # vertical line
-            self.canvas.create_line(i, 0, i, self.canvas_scale)
-
-    # creates a matrix of rectangle objects grid[row][column]
-    def create_grid(self):
-        self.grid = []
-        for row in range(0, self.step):
-            x0 = row * self.step
-            x1 = x0 + self.step
-            column = []
-            for col in range(0, self.step):
-                y0 = col * self.step
-                y1 = y0 + self.step
-                column.append(self.canvas.create_rectangle(x0, y0, x1, y1, fill=self.color_floor, width=0))
-            self.grid.append(column)
+            self.canvas.create_line(i, 0, i, self.canvas_width, tags=("grid_lines"))
+    
+    def hide_grid_lines(self):
+        if self.show_grid.get() == 0:
+            self.canvas.itemconfigure("grid_lines", state="hidden")
+        elif self.show_grid.get() == 1:
+            self.canvas.itemconfigure("grid_lines", state="normal")
 
     # changes colour of rectangle object from grid
-    def draw_shelf(self, row, col, color):
-        self.canvas.itemconfigure(self.grid[row][col], fill=color)
+    def draw_shelf(self, row, col):
+        x0 = row * self.step
+        x1 = x0 + self.step
+        y0 = col * self.step
+        y1 = y0 + self.step
+        shelf = self.canvas.create_rectangle(x0, y0, x1, y1, fill=self.color_shelf, width=0)
+        self.canvas.tag_bind(shelf, '<Button-1>', self.onShelfClick)
+    
+    # deletes shelf object when clicked on the shelf
+    def onShelfClick(self, event):
+        self.canvas.delete(event.widget.find_withtag('current')[0])
 
-    # onClick event on canvas
+    # draws shelf when clicked on the canvas and when drawing tool selected
     def onCanvasClick(self, event):
         row = int(event.x / self.step)
         col = int(event.y / self.step)
-        if self.tool_selec.get() == 0:
-            self.draw_shelf(row=row, col=col, color=self.color_floor)
-        elif self.tool_selec.get() == 1:
-            self.draw_shelf(row=row, col=col, color=self.color_shelf)
+        if self.tool_selec.get() == 1:
+            self.draw_shelf(row=row, col=col)
 
+    # save the canvas as .png and return its file name
     def saveCanvas(self):
-        fileName = "storeMap"
+        fileName = "storeMap.png"
         preMadeLayout = "ExampleSuperMarket.png"
-        # POGING 1
-        # ps = self.canvas.postscript(colormode = 'color')
-        # img = Image.open(io.BytesIO(ps.encode('utf-8')))
-        # img.save(fileName + '.png')
+        
+        # create postscript image from canvas
+        ps = self.canvas.postscript(colormode="mono", pageheight='101', pagewidth='101')
+        # grab postscript from IO and save as .png
+        Image.open(io.BytesIO(ps.encode('utf-8'))).save(fileName)
 
-        # POGING 2
-        # save postscipt image 
-        # self.canvas.postscript(file = fileName + '.ps', colormode='color') 
-        # # use PIL to convert to PNG 
-        # img = Image.open(fileName + '.ps') 
-        # img.save(fileName + '.pbm') 
-
-        # POGING 3
-        # x=self.canvas.winfo_rootx() + self.canvas.winfo_x()
-        # y=self.canvas.winfo_rooty() + self.canvas.winfo_x()
-        # x1=x + self.canvas_scale
-        # y1=y + self.canvas_scale
-        # ImageGrab.grab().crop((x,y,x1,y1)).save(fileName + ".pbm")
-
-        # POSING 4
-        # HWND = self.canvas.winfo_id()  # get the handle of the canvas
-        # rect = win32gui.GetWindowRect(HWND)  # get the coordinate of the canvas
-        # ImageGrab.grab(rect).save(fileName + ".png")  # get image of the current location
-
-        return str(preMadeLayout)
+        return str(fileName)
 
 
