c59690356fad35e245b74269b5d100377894e478
Merel <mereleikens@gmail.com>
2021-03-18 18:50:52 +0100

    remove old outputfunction from original gui file

--- MonteCarlo/Gui.py
+++ MonteCarlo/Gui.py
@@ -118,13 +118,6 @@ class Gui:
         for f in os.listdir(dir):
             os.remove(os.path.join(dir, f))
 
-    # updates output window after simulation is done
-    # def update_output_window(self):
-    #     self.txt_output.config(cursor='arrow')
-    #     self.txt_output.pack()
-    #     self.update_output("Simulation finished!")
-    #     # More stuff TODO after simulation
-
     def run_simulation(self, seed, nr_customers, max_steps, prob_inf, prob_new, store_layout):
         # clear the figures of previous simulations
         self.clear_folder()
@@ -143,24 +136,6 @@ class Gui:
                                     max_steps=max_steps, 
                                     prob_inf=prob_inf, 
                                     prob_new=prob_new)
-            # outputGui.draw_output_window()
-            # self.label = self.draw_output_window()
-
-            # self.update_output("Running simulation with the following parameters:")
-            # self.update_output(" Seed:                    " + seed)
-            # self.update_output(" Nr. of Customers:        " + nr_customers)
-            # self.update_output(" Max Steps:               " + max_steps)
-            # self.update_output(" Prob. Infected Customer: " + prob_inf)
-            # self.update_output(" Prob. New Customer:      " + prob_new)
-
-            # # Stream video
-            # def stream():
-            #     video = imageio.get_reader("video.mkv")
-            #     for image in video.iter_data():
-            #         image_frame = Image.fromarray(image)          
-            #         frame_image = ImageTk.PhotoImage(image_frame.resize((int(self.window_width/2), int(self.window_height/1.5))))
-            #         self.label.config(image=frame_image)
-            #         self.label.image = frame_image
 
             def run_sim():
                 sim = Simulation(
@@ -180,7 +155,6 @@ class Gui:
                 sim.runSimulation()
                 self.simulating = False
                 outputGui.update_output_window()
-                # outputGui.stream
             
             # load the already simulated figures
             def load_figures():
@@ -200,59 +174,3 @@ class Gui:
             t.setDaemon(True)
             t.start()
         
-    # #updates simulation frame
-    # def update_figure(self, label, id, step):
-    #     image = Image.open('simFigures/simFigure_%d_%07d.png'%(id, step))
-    #     img = ImageTk.PhotoImage(image.resize((int(self.window_width/2), int(self.window_height/1.5))))
-    #     label.configure(image=img)
-    #     label.image = img
-
-    # # Output
-    # def draw_output_window(self):
-    #     self.output_line_nr = 0
-
-    #     window = tk.Toplevel()
-    #     window.geometry('{}x{}'.format(self.window_width, self.window_height))
-
-    #     # Simulation frame
-    #     frm_sim = tk.Frame(window, height=self.window_height/2, width=self.window_width/2, bg="red")
-
-    #     lbl_id_sim = tk.Label(frm_sim, text="Simulation Frame")
-    #     lbl_id_sim.pack()
-    #     btn_frame = tk.Button(frm_sim, text="Show simulation frame", command=lambda: self.update_figure(lbl_id_sim, 888892, 1))
-    #     btn_frame.pack()
-
-    #     # Output frame
-    #     frm_output = tk.Frame(window, height=self.window_height/2, width=self.window_width/2, bg="yellow")
-
-    #     lbl_id_parameters = tk.Label(frm_output, text="Output Frame")
-    #     lbl_id_parameters.pack()
-
-    #     self.txt_output = tk.Text(frm_output, height=15, width=70, cursor='watch')
-    #     self.txt_output.config(wrap='none', state='disabled')
-    #     self.txt_output.pack()
-
-    #     frm_sim.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
-    #     frm_output.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True)
-
-    #     return lbl_id_sim
-
-    # def update_output(self, line):
-    #     if self.txt_output == 0:
-    #         raise Exception("Output text is undefined")
-    #     else:
-    #         # Construct output line
-    #         output_line = ""
-    #         if self.txt_output.get("1.0", tk.END) != "\n":
-    #             output_line += "\n"
-
-    #         # output_line += str(self.output_line_nr) + ": " + line
-    #         output_line += " " + line
-
-    #         # Update output
-    #         self.txt_output.config(state='normal')
-    #         self.txt_output.insert(tk.END, output_line)
-    #         self.txt_output.see('end -1 lines')
-    #         self.txt_output.config(state='disabled')
-
-    #         self.output_line_nr += 1
