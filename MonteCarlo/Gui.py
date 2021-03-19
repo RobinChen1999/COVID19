3a2d4e2a39cc3ad9e789a188603874718f2d343f
Merel <mereleikens@gmail.com>
2021-03-19 18:02:37 +0100

    merch fix

--- MonteCarlo/Gui.py
+++ MonteCarlo/Gui.py
@@ -1,21 +1,10 @@
-import tkinter as tk
-import threading
-import imageio
-import shutil
-import Params
-import os
-
-from pathlib import Path
-
 from tkinter import *
-from PIL import Image, ImageTk
+from tkinter import messagebox
+from tkinter import ttk
+import Params
 from Simulation import *
 from StoreLayout import *
 from GuiOutput import *
-from tkinter import messagebox
-from tkinter import ttk
-from tkinter.filedialog import asksaveasfile
-
 
 class Gui:
     def __init__(self):
@@ -240,15 +229,6 @@ class Gui:
         for f in os.listdir(dir):
             os.remove(os.path.join(dir, f))
 
-    # opens a 'save-as' window to save the video
-    # def save_file(self):
-    #     file = asksaveasfile(mode="wb", title="Save Figure", defaultextension=".mkv", filetypes = (("mkv files",".mkv"),("all files",".*")))
-    #     if file is None:
-    #         return None
-    #     vid_to_save = open("video.mkv","rb").read()
-    #     file.write(vid_to_save)
-    #     file.close()
-
     def run_simulation(self, simulation_params, customer_params, exit_params, diffusion_params, plume_params,
                        store_layout):
         self.simulating = True
@@ -264,12 +244,7 @@ class Gui:
         )
 
         if input_valid:
-            outputGui = GuiOutput()
-                # seed=simulation_params["seed"], 
-                #                     nr_customers=nr_customers, 
-                #                     max_steps=max_steps, 
-                #                     prob_inf=prob_inf, 
-                #                     prob_new=prob_new)
+            outputGui = GuiOutput(simulation_params)
 
             def run_sim():
                 # Update global params
@@ -300,101 +275,11 @@ class Gui:
                     useDiffusion=1,
                     dx=1.0)
                 sim.runSimulation()
-                self.simulating = False
+                outputGui.simulating = False
                 outputGui.update_output_window()
-            
-            # load the already simulated figures
-            def load_figures():
-                while self.simulating:
-                    # when simFigures has at least 2 figures
-                    if len(os.listdir('simFigures')) > 1:
-                        figureList = glob.glob('simFigures/simFigure_%s_*' % simulation_params["seed"] + '.png')
-                        latest_figure = figureList[-2]  # get second last element
-                        img = Image.open(latest_figure)
-                        frm_img = ImageTk.PhotoImage(img.resize((int(self.window_width/2), int(self.window_height/1.5))))
-                        outputGui.label.config(image=frm_img)
-                        outputGui.label.image = frm_img
 
             # Start simulation in new thread so GUI doesn't block
             threading.Thread(target=run_sim).start()
-            t = threading.Thread(target=load_figures)
-            t.setDaemon(True)
-            t.start()
-
-    # # Output
-    # def draw_output_window(self):
-    #     self.output_line_nr = 0
-
-    #     window = tk.Toplevel()
-    #     window.state('zoomed')
-
-    #     # Simulation frame
-    #     self.frm_sim = tk.Frame(window, height=self.window_height / 2, width=self.window_width / 2, bg="red")
-
-    #     lbl_id_sim = tk.Label(self.frm_sim, text="Simulation Frame")
-    #     lbl_id_sim.pack()
-    #     self.lbl_sim = tk.Label(self.frm_sim, cursor='watch')
-    #     self.lbl_sim.pack()
-
-    #     # Output frame
-    #     frm_output = tk.Frame(window, height=self.window_height / 2, width=self.window_width / 2, bg="yellow")
-
-    #     lbl_id_parameters = tk.Label(frm_output, text="Output Frame")
-    #     lbl_id_parameters.pack()
-
-    #     self.txt_output = tk.Text(frm_output, height=15, width=70, cursor='watch')
-    #     self.txt_output.config(wrap='none', state='disabled')
-    #     self.txt_output.pack()
-
-    #     self.txt_step_output = tk.Text(frm_output, height=5, width=70)
-    #     self.txt_step_output.config(wrap='none', state='disabled')
-    #     self.txt_step_output.pack()
-
-    #     self.frm_sim.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
-    #     frm_output.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True)
-
-    #     return self.lbl_sim
-
-    # def update_output(self, line):
-    #     if self.txt_output == 0:
-    #         raise Exception("Output text is undefined")
-    #     else:
-    #         # Construct output line
-    #         output_line = ""
-    #         if self.txt_output.get("1.0", tk.END) != "\n":
-    #             output_line += "\n"
-
-    #         # output_line += str(self.output_line_nr) + ": " + line
-    #         output_line += " " + line
-
-    #         # Update output
-    #         self.txt_output.config(state='normal')
-    #         self.txt_output.insert(tk.END, output_line)
-    #         self.txt_output.see('end -1 lines')
-    #         self.txt_output.config(state='disabled')
-
-    #         self.output_line_nr += 1
-
-    # def update_displayed_step(self, step, customers_in_store=-1, customers_in_queue=-1, emitting_customers_in_store=-1,
-    #                           exposure=-1):
-    #     if self.txt_step_output == 0:
-    #         raise Exception("Step output text is undefined")
-    #     else:
-    #         if any(x == -1 for x in [customers_in_store, customers_in_queue, emitting_customers_in_store, exposure]):
-    #             print("todo")
-    #             # TODO: Get data from store_data.dat file
-
-    #         output = " Step: {}\n" \
-    #                  "  Customers in store:            {:.0f}\n" \
-    #                  "  Customers heading for exit:    {:.0f}\n" \
-    #                  "  Infected customers:            {:.0f}\n" \
-    #                  "  Exposure on healthy customers: {:.3f}".format(step, customers_in_store, customers_in_queue,
-    #                                                                   emitting_customers_in_store, exposure)
-
-    #         self.txt_step_output.config(state='normal')
-    #         self.txt_step_output.delete('1.0', tk.END)
-    #         self.txt_step_output.insert(tk.END, output)
-    #         self.txt_step_output.config(state='disabled')
 
 
 class ToolTip(object):
