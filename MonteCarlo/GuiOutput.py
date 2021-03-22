4347adcb2354f8cdd40d9abd17b844443c937042
Joris Peters <petersjoris98@gmail.com>
2021-03-22 15:04:24 +0100

    Autoformatter

--- MonteCarlo/GuiOutput.py
+++ MonteCarlo/GuiOutput.py
@@ -1,20 +1,14 @@
-import time
-
-from StorePlot import *
-
 import tkinter as tk
 import threading
-import imageio
-# import shutil
 import glob
 import os
 from PIL import Image, ImageTk
 
-import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 from tkinter.filedialog import asksaveasfile
+
 
 class GuiOutput:
     window_width = 1600
@@ -26,26 +20,25 @@ class GuiOutput:
         self.update_output("Running simulation with the following parameters:")
         self.max_steps = simulation_params["max_steps"]
         self.seed = simulation_params["seed"]
-        
+
         # Start load_figures in new thread so GUI doesn't block
         t = threading.Thread(target=self.load_figures)
         t.setDaemon(True)
         t.start()
-
-        
 
     # updates output window after simulation is done
     def update_on_sim_finished(self, store_plot):
         self.txt_output.config(cursor='arrow')
         self.txt_output.pack()
         self.update_output("Simulation finished!")
-        
+
         self.store_plot = store_plot
         self.store_plot.init_canvas(canvas=self.canvas_sim, store_fig=self.lbl_sim, height=self.canvas_height)
 
         steps = int(self.max_steps) - 1
-        length = max(int((self.window_width/2)/steps), 10)
-        slider = tk.Scale(self.frm_sim, from_=0, to=steps, length=int(self.canvas_height), sliderlength=length, orient=tk.HORIZONTAL, command=self.slider_handler)
+        length = max(int((self.window_width / 2) / steps), 10)
+        slider = tk.Scale(self.frm_sim, from_=0, to=steps, length=int(self.canvas_height), sliderlength=length,
+                          orient=tk.HORIZONTAL, command=self.slider_handler)
         slider.pack()
         btn_export = tk.Button(self.frm_sim, text="Export video", command=lambda: self.save_file())
         btn_export.pack()
@@ -58,20 +51,21 @@ class GuiOutput:
 
     # opens a 'save-as' window to save the video
     def save_file(self):
-        file = asksaveasfile(mode="wb", title="Save Figure", defaultextension=".mkv", filetypes = (("mkv files",".mkv"),("all files",".*")))
+        file = asksaveasfile(mode="wb", title="Save Figure", defaultextension=".mkv",
+                             filetypes=(("mkv files", ".mkv"), ("all files", ".*")))
         if file is None:
             return None
-        vid_to_save = open("video.mkv","rb").read()
+        vid_to_save = open("video.mkv", "rb").read()
         file.write(vid_to_save)
         file.close()
-    
+
     # load the already simulated figures
     def load_figures(self):
         while self.simulating:
             # when simFigures has at least 2 figures
             if len(os.listdir('simFigures')) > 1:
-                figureList = glob.glob('simFigures/simFigure_%s_*'%self.seed + '.png')
-                latest_figure = figureList[-2] # get second last element  
+                figureList = glob.glob('simFigures/simFigure_%s_*' % self.seed + '.png')
+                latest_figure = figureList[-2]  # get second last element
                 img = Image.open(latest_figure)
                 self.frm_img = ImageTk.PhotoImage(img.resize((int(self.canvas_height), int(self.canvas_height))))
                 self.canvas_sim.itemconfig(self.lbl_sim, image=self.frm_img)
@@ -88,8 +82,8 @@ class GuiOutput:
 
         lbl_id_sim = tk.Label(self.frm_sim, text="Simulation Frame")
         lbl_id_sim.pack()
-        
-        self.canvas_height = height=self.window_height / 3*2
+
+        self.canvas_height = height = self.window_height / 3 * 2
         self.canvas_sim = tk.Canvas(self.frm_sim, height=self.canvas_height, width=self.canvas_height)
         self.canvas_sim.pack()
         self.lbl_sim = self.canvas_sim.create_image(0, 0, anchor="nw")
@@ -139,7 +133,6 @@ class GuiOutput:
 
         self.frm_sim.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
         frm_output.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True)
-
 
     def update_output(self, line):
         if self.txt_output == 0:
@@ -219,4 +212,3 @@ class GuiOutput:
         self.ax_exposure.plot(step, self.ax_exposure.lines[0].get_ydata()[step], 'go')
 
         self.canvas.draw_idle()
-
